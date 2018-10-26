b9c72b9b5e4527c538bda8e777b309009a3b4f53
Chiza Barber <chiza.alba@gmail.com>
2018-10-25 21:09:25 -0500

    Nametags complete

--- renderNametags.js
+++ renderNametags.js
@@ -1,10 +1,18 @@
-
-function renderNametags(nametags) {
-    return `
+function renderNametags(nametag) {
+	var finalHTML = ''
+	var namesHTML = nametag.map(function (nametags) {
+		var nameHTML = `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(nametags)}</code>
-        </div>
+             <div style="width:200px;height:40px;background-color:darkblue;color:white;margin:auto;">Hello, my name is:</div>
+             <div style="width:200px;height:100px;border:1px solid black;margin:auto;">${nametags}</div>
+         </div>
     `
+		return nameHTML;
+	});
+
+	finalHTML += namesHTML.join('');
+
+    return finalHTML;
 }
 
 function nametags() {
